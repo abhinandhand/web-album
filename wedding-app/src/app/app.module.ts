fa8f2e23291ae0dbb19d17929a6882355fbf6b0c
Abhinandhan Devadiga <abhi@Abhinandhans-MacBook-Air.local>
2019-03-14 00:00:23 +0530

    navbar

--- wedding-app/src/app/app.module.ts
+++ wedding-app/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
@@ -37,7 +40,8 @@ const appRoutes: Routes   = [ ];
     AppRoutingModule,
     HttpClientModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
